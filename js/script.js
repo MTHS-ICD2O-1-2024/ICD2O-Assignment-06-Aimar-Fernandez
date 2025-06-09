66f2c26fcb6a365dbe5dec2d26e0650bee1fb975
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-06-09 17:01:54 +0000

    added api usage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Jun 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+async function updateDay() {
+  try {
+    const infoJSON = await fetch("https://worldtimeapi.org/api/timezone/America/Toronto")
+    const JSONdata = await infoJSON.json()
+    const dayOfYear = await JSONdata.day_of_year
+    document.getElementById("answer").innerHTML = 
+      "<p>" + dayOfYear + " / 365 days through the year</p>"
+  }
+  catch (error) {
+    document.getElementById("answer").innerHTML = "<p>Error</p>"
+  }
+}
